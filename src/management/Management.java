fbe9f71dc1a5bee5218c6e4b0ca87957dd12e11e
Sang Won Lee <freerusya@naver.com>
2024-09-20 00:52:53 +0900

    feat: 일정 완료처리 기능

--- src/management/Management.java
+++ src/management/Management.java
@@ -12,8 +12,23 @@ public class Management {
     void addTodo() {
 
     }
-    void markTodoComplete() {
-
+    void markTodoComplete(String title) {
+        boolean find = false;
+        for (Todo todo : todoList) {
+            if (todo.getTitle().equals(title)) {
+                if (todo.getIsComplete()) {
+                    System.out.println("해당일정은 완료되어있습니다.");
+                } else {
+                    todo.setIsComplete(true);
+                    System.out.println("일정 완료 처리 되었습니다.");
+                }
+                find = true;
+                break;
+            }
+        }
+        if (!find) {
+            System.out.println("존재하지 않는 일정입니다.");
+        }
     }
     void printTodoList() {
 
