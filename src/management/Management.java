8f1bc7539920a2b65031d3c81df40d57d15e4b45
Sang Won Lee <freerusya@naver.com>
2024-09-20 00:16:28 +0900

    feat: 목록에서 할일 제거 기능

--- src/management/Management.java
+++ src/management/Management.java
@@ -18,8 +18,18 @@ public class Management {
     void printTodoList() {
 
     }
-    void removeTodo() {
-
+    void removeTodo(Todo title) {
+        boolean find = false;
+        for (Todo todo : todoList) {
+            if (todo.getTitle().equals(title)) {
+                todoList.remove(todo);
+                System.out.println("할일 " + todo + "를(을) list에서 삭제했습니다.");
+                find = true;
+            }
+        }
+        if (!find) {
+            System.out.println("존재 하지 않는 " + title + "입니다.");
+        }
     }
     void printFamily() {
 
