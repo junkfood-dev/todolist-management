15d826edaf60380fe15e8f997e0577b5ac2f401a
ejjeong9103 <ejjeong9103@naver.com>
2024-09-21 22:53:07 +0900

    feat: 사라진 member클래스, todo클래스 부활

--- src/management/Member.java
+++ src/management/Member.java
@@ -4,10 +4,14 @@ public class Member {
     String name;
     String role;
 
+    Member(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
     String getName() {
-
+        return this.name;
     }
     String getRole() {
-
+        return this.role;
     }
 }
--- src/management/Todo.java
+++ src/management/Todo.java
@@ -4,21 +4,27 @@ public class Todo {
     String title;
     String date;
     String owner;
-    boolean isComplete;
+    boolean isComplete = false;
 
-    void setIsComplete() {
+    Todo(String title, String date, String owner) {
+        this.title = title;
+        this.date = date;
+        this.owner = owner;
+    }
 
+    void setIsComplete(boolean isComplete) {
+        this.isComplete = isComplete;
     }
     boolean getIsComplete() {
-
+        return isComplete;
     }
     String getTitle() {
-
+        return title;
     }
     String getDate() {
-
+        return date;
     }
     String getOwner() {
-
+        return owner;
     }
 }
